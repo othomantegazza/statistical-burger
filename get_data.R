45290d99430b4049b63259ef92705c5e5d896a47
Otho <otho.mantegazza@gmail.com>
2020-02-11 19:28:12 +0100

    overfitting and logit

--- get_data.R
+++ get_data.R
@@ -1,2 +1,5 @@
 download.file("https://raw.githubusercontent.com/nguyen-toan/ISLR/master/dataset/Advertising.csv",
               destfile = here::here("data", "adv.csv"))
+
+download.file("https://raw.githubusercontent.com/nguyen-toan/ISLR/master/dataset/Income1.csv",
+              destfile = here::here("data", "income1.csv"))
